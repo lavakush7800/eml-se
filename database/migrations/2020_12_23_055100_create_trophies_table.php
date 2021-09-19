18f6332a6886c04856d3e6914e93dd4be39b0a3e
lavakush7800 <lavakushgiri@gmail.com>
2021-09-19 21:10:12 +0530

    modify code

--- database/migrations/2020_12_23_055100_create_trophies_table.php
+++ database/migrations/2020_12_23_055100_create_trophies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateTrophiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('trophies', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
         });
     }
 
@@ -26,6 +26,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('trophies');
     }
 }
