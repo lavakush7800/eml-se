c9c1547fd49f589e00a8646373fa4781a1628db3
lavakush7800 <lavakushgiri@gmail.com>
2021-09-21 21:33:35 +0530

    modify code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,12 +39,21 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                       <li>
-                           <a class="nav-link" href="/user">Home</a>
-                        </li>
-                        <li>
-                           <a class="nav-link" href="/user">Book</a>
-                        </li>
+                           <li class="nav-item">
+                                <a class="nav-link" href="/home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="book_show">Book</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/author">Authors</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/category">Category</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/publisher">Publisher</a>
+                            </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -80,6 +89,8 @@
         </nav>
 
         <main class="py-4">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
             @yield('content')
         </main>
     </div>
